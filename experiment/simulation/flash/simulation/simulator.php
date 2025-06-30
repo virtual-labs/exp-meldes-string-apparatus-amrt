38c7f33ae004733be8d3a141e9ac400f9386d1c9
saneeshpfrancis <saneeshpf@gmail.com>
2025-06-30 13:45:07 +0530

    Simulator and figures added

---
+++ experiment/simulation/flash/simulation/simulator.php
@@ -0,0 +1,19 @@
+<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+<div class="post" align="left">&nbsp; <!--leave this space as such.. som wired issue-->
+<?php include('breadcum.php'); ?>
+
+  <blockquote> 
+    <span class="title">MeldesExperiment</span>
+    
+	<p>    <div class="postConentPadding">
+	  <?php
+		//@include('vlab/'.$sub.'/'.$brch.'/'.$sim.'/menu.php');
+		@include('menu.php');
+
+?>
+</p>
+<p align="center"><iframe src="<?= 'vlab/'.$sub.'/'.$brch.'/'.$sim.'/'.'MeldesExperiment.html'?>" frameborder="0" height="600" width="800" scrolling="no"></iframe></p>
+  
+
+</blockquote>&nbsp;
+</div>
